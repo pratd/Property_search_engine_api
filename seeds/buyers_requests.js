f3d9283ac5d8ca1acbe9f3302cf9a50b54bef631
Cristina Moreno Medran <moreno.medran@gmail.com>
2020-04-13 15:44:19 +0200

    buyers requests seeders done in here

--- seeds/buyers_requests.js
+++ seeds/buyers_requests.js
@@ -2,7 +2,35 @@ const Buyer_request = require('../models/buyers_requests');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/lookhaus', { useNewUrlParser: true , useUnifiedTopology: true });
 
-const users = [{
+const buyers_requests = [{
+	"property_id":"5e943c073c6ce8c679632b05",
+	"seller_id":'5e9059ef7a856cf23b825502',
+	"buyer_name":"lorem ipsum",
+	"buyer_email":"lorea@abc.com",
+	"buyer_message":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+	"property_id":"5e943c073c6ce8c679632b05",
+	"seller_id":'5e9059ef7a856cf23b825502',
+	"buyer_name":"lorem ipsum",
+	"buyer_email":"lorea@abc.com",
+	"buyer_message":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+	"property_id":"5e943c073c6ce8c679632b05",
+	"seller_id":'5e9059ef7a856cf23b825502',
+	"buyer_name":"lorem ipsum",
+	"buyer_email":"lorea@abc.com",
+	"buyer_message":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+	"property_id":"5e943c073c6ce8c679632b05",
+	"seller_id":'5e9059ef7a856cf23b825502',
+	"buyer_name":"lorem ipsum",
+	"buyer_email":"lorea@abc.com",
+	"buyer_message":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
 	"property_id":"5e943c073c6ce8c679632b05",
 	"seller_id":'5e9059ef7a856cf23b825502',
 	"buyer_name":"lorem ipsum",
@@ -18,58 +46,30 @@ const users = [{
   },
   {
 	"property_id":"5e943c073c6ce8c679632b05",
-	"seller_id":2,
-	"buyer_name":"lorem ipsum",
-	"buyer_email":"lorea@abc.com",
-	"buyer_message":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-  },
-  {
-	"property_id":"5e943c073c6ce8c679632b05",
-	"seller_id":2,
-	"buyer_name":"lorem ipsum",
-	"buyer_email":"lorea@abc.com",
-	"buyer_message":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-  },
-  {
-	"property_id":"5e943c073c6ce8c679632b05",
-	"seller_id":2,
-	"buyer_name":"lorem ipsum",
-	"buyer_email":"lorea@abc.com",
-	"buyer_message":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-  },
-  {
-	"property_id":"5e943c073c6ce8c679632b05",
-	"seller_id":2,
-	"buyer_name":"lorem ipsum",
-	"buyer_email":"lorea@abc.com",
-	"buyer_message":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-  },
-  {
-	"property_id":"5e943c073c6ce8c679632b05",
-	"seller_id":2,
+	"seller_id":'5e9059ef7a856cf23b825502',
 	"buyer_name":"lorem ipsum",
 	"buyer_email":"lorea@abc.com",
 	"buyer_message":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
   }
 ]
 
-function deleteAllBuyers() {
-	return Buyer.deleteMany({})
+function deleteAllBuyersRequests() {
+	return Buyer_request.deleteMany({})
 		.then(() => {
-			console.log('Deleted all users');
+			console.log('Deleted all buyer\'s requests');
 		})
 		.catch((err) => {
-			console.log('Failed to delete all users');
+			console.log('Failed to deleted all buyer\'s requests');
 			return Promise.reject(err);
 		});
 }
 
-deleteAllBuyers();
+deleteAllBuyersRequests();
 
-Buyer.create(buyers, (err, buyers) => {
+Buyer_request.create(buyers_requests, (err, buyers_requests) => {
 	if (err) {
 		throw (err);
 	}
-	console.log('Success', buyers);
+	console.log('Success', buyers_requests);
 	mongoose.connection.close();
 });
