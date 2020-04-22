7093e45026c5cd89912ec26005b43f0d3a2533eb
Cristina Moreno Medran <moreno.medran@gmail.com>
2020-04-22 20:56:54 +0200

    file upload works here

--- routes/createHome.js
+++ routes/createHome.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const server = require("../index.js");
 const homeSchema = require("../models/homes");
 require("dotenv").config();
 const secretKey = process.env.SECRET;
@@ -10,29 +12,65 @@ module.exports = {
     //   strategy: "jwtokenization",
     //   scope: ["user"],
     // },
+    payload: {
+      output: "stream",
+      parse: true,
+      allow: [
+        "application/json",
+        "multipart/form-data",
+        "image/jpeg",
+        "application/pdf",
+        "application/x-www-form-urlencoded",
+      ],
+      //   multipart: true,
+      maxBytes: 1024 * 1024 * 100,
+      timeout: false,
+    },
     handler: async (req, res) => {
+      const data = req.payload;
+
+      // console.log(req.payload.photos);
+      if (data.photos) {
+        const name = data.photos.hapi.filename;
+        const path = __dirname + "/../uploads/" + name;
+        const file = fs.createWriteStream(path);
+
+        file.on("error", (err) => console.error(err));
+
+        data.photos.pipe(file);
+
+        data.photos.on("end", (err) => {
+          const ret = {
+            filename: data.photos.hapi.filename,
+            headers: data.photos.hapi.headers,
+          };
+          return JSON.stringify(ret);
+        });
+      }
+
       var home = new homeSchema({
-        name: req.payload.name,
-        description: req.payload.description,
-        kind: req.payload.kind,
-        location: req.payload.location,
-        bedrooms: req.payload.bedrooms,
-        bathrooms: req.payload.bathrooms,
-        kitchen: req.payload.kitchen,
-        condition: req.payload.condition,
-        price: req.payload.price,
-        lift: req.payload.lift,
-        pets_allowed: req.payload.pets_allowed,
-        garden: req.payload.garden,
-        swimming_pool: req.payload.swimming_pool,
-        air_conditioning: req.payload.air_conditioning,
-        air_conditioning: req.payload.air_conditioning,
-        heating: req.payload.heating,
-        floor: req.payload.floor,
-        orientation: req.payload.orientation,
-        energy_certificate: req.payload.energy_certificate,
-        parking: req.payload.parking,
-        bargain: req.payload.bargain,
+        name: data.name,
+        photos: server.info.uri + "/uploads/" + data.photos.hapi.filename,
+        description: data.description,
+        kind: data.kind,
+        location: data.location,
+        bedrooms: data.bedrooms,
+        bathrooms: data.bathrooms,
+        kitchen: data.kitchen,
+        condition: data.condition,
+        price: data.price,
+        lift: data.lift,
+        pets_allowed: data.pets_allowed,
+        garden: data.garden,
+        swimming_pool: data.swimming_pool,
+        air_conditioning: data.air_conditioning,
+        air_conditioning: data.air_conditioning,
+        heating: data.heating,
+        floor: data.floor,
+        orientation: data.orientation,
+        energy_certificate: data.energy_certificate,
+        parking: data.parking,
+        bargain: data.bargain,
       });
       try {
         await home.save();
