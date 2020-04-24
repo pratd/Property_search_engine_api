13fe67e6eea4f35da5bb36e726cb686debc9db8e
Cristina Moreno Medran <cmoreno@redpoints.com>
2020-04-24 18:27:02 +0200

    adding endpoint to check conflict with uploads dir

--- routes/uploads.js
+++ routes/uploads.js
@@ -4,12 +4,16 @@ const path = require("path");
 
 module.exports = {
   method: "GET",
-  path: "/uploads/{name}",
+  path: "/uploads",
   config: {
     //!This handler needs refactor
     handler: async (req, res) => {
-      const filename = req.params.name;
-      return res.file(filename);
+      if (!fs.existsSync(__dirname + "/../uploads/")) {
+        const uploads = fs.readdir(__dirname + "/../uploads/");
+        return res.response(uploads);
+      } else {
+        return res.response("Uploads dir doesn't exist yet");
+      }
     },
   },
 };
